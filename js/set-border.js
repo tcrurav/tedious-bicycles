06529d4646e90daae55acd788c0aedfdab8993c4
tcrurav <tiburcio.cruz@gmail.com>
2025-10-24 16:43:10 +0100

    first commit

---
+++ js/set-border.js
@@ -0,0 +1,83 @@
+function listenToClickOnImage(){
+    let image01 = document.getElementById("image-01");
+    image01.addEventListener("click", setBorder01);
+
+    let image02 = document.getElementById("image-02");
+    image02.addEventListener("click", setBorder02);
+
+    let image03 = document.getElementById("image-03");
+    image03.addEventListener("click", setBorder03);
+
+    let image04 = document.getElementById("image-04");
+    image04.addEventListener("click", setBorder04);
+
+    let image05 = document.getElementById("image-05");
+    image05.addEventListener("click", setBorder05);
+
+    let image06 = document.getElementById("image-06");
+    image06.addEventListener("click", setBorder06);
+
+    let image07 = document.getElementById("image-07");
+    image07.addEventListener("click", setBorder07);
+
+    let image08 = document.getElementById("image-08");
+    image08.addEventListener("click", setBorder08);
+
+    let image09 = document.getElementById("image-09");
+    image09.addEventListener("click", setBorder09);
+
+    let image10 = document.getElementById("image-10");
+    image10.addEventListener("click", setBorder10);
+}
+
+function setBorder01(){
+    let image01 = document.getElementById("image-01");
+    image01.style.border = "3px solid black";
+}
+
+function setBorder02(){
+    let image02 = document.getElementById("image-02");
+    image02.style.border = "3px solid black";
+}
+
+function setBorder03(){
+    let image03 = document.getElementById("image-034");
+    image03.style.border = "3px solid black";
+}
+
+function setBorder04(){
+    let image04 = document.getElementById("image-04");
+    image04.style.border = "3px solid black";
+}
+
+function setBorder05(){
+    let image05 = document.getElementById("image-05");
+    image05.style.border = "3px solid black";
+}
+
+function setBorder06(){
+    let image06 = document.getElementById("image-06");
+    image06.style.border = "3px solid black";
+}
+
+function setBorder07(){
+    let image07 = document.getElementById("image-07");
+    image07.style.border = "3px solid black";
+}
+
+function setBorder08(){
+    let image08 = document.getElementById("image-08");
+    image08.style.border = "3px solid black";
+}
+
+function setBorder09(){
+    let image09 = document.getElementById("image-09");
+    image09.style.border = "3px solid black";
+}
+
+function setBorder10(){
+    let image10 = document.getElementById("image-10");
+    image10.style.border = "3px solid black";
+}
+
+listenToClickOnImage();
